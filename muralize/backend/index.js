cd1794e011fb0a31245e30c0026bfb3091d585c1
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-05-22 12:58:12 -0300

    uptade server

--- muralize/backend/index.js
+++ muralize/backend/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const db = require('./config/db')
+const routes = require('./routes')
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.use(express.json())
+app.use('/api', routes)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${port}`)
 })

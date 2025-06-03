d938d7efa2deb6ad1e0d364e6520e904df35c18f
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-06-03 15:06:47 -0300

    style: alter style of db.js

--- muralize/database/db.js
+++ muralize/database/db.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql2'); 
-const connection = mysql.createConnection( {
-host: 'localhost', 
-user: 'root', 
-password: 'root', 
-database: 'muralize' 
-} );
 
-connection.connect(
-function(err) { 
-if (err) {
-console.error('Erro ao conectar ao banco de dados: ' + err.stack);
-return;
-}
-console.log('Conexão bem-sucedida com o ID: ' + connection.threadId);
+const connection = mysql.createConnection( {
+    host: 'localhost', 
+    user: 'root', 
+    password: 'root', 
+    database: 'muralize' 
 });
 
+connection.connect(
+    function(err) { 
+        if (err) {
+            console.error('Erro ao conectar ao banco de dados: ' + err.stack);
+            return;
+        }
+        console.log('Conexão bem-sucedida com o ID: ' + connection.threadId);
+    });
